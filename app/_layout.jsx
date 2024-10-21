59f9ff28330472c498af5aa9eab2da1c348ea47f
Suraj Dey <surajdey2k1@gmail.com>
2024-10-22 00:45:05 +0530

    react-native-google-places-autocomplete  - Version Downgraded
    react-native-google-places-autocomplete - Version downgraded from 2.5.7 to 2.5.6 as the latest version is showing a bug in which - ''crypto.getRandomValues() not supported'' is persistent. Issue resolved and search bar is functioning correctly. Data stored using useState  for future use as this data will be delivered to gemini for planning trip ...

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,8 @@
 import { Stack } from "expo-router";
 import {useFonts} from "expo-font"
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import {CreateTripContext} from '../context/CreateTripContext'
+import { useState } from "react";
 
 export default function RootLayout() {
   useFonts({
@@ -8,12 +10,15 @@ export default function RootLayout() {
     'outfit-medium':require('./../assets/fonts/Outfit-Medium.ttf'),
     'outfit-bold':require('./../assets/fonts/Outfit-Bold.ttf'),
   })
+  const [tripData, setTripData] = useState([]);
   return (
     <GestureHandlerRootView>
+    <CreateTripContext.Provider value={{tripData,setTripData}}>
     <Stack screenOptions={{headerShown:false}}>
       {/* <Stack.Screen name="index"/> */}
       <Stack.Screen name="(tabs)"/>
     </Stack>
+    </CreateTripContext.Provider>
     </GestureHandlerRootView>
   );
 }
--- app/create-trip/search-place.jsx
+++ app/create-trip/search-place.jsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Colors } from '../../constants/Colors'
 import { useNavigation } from 'expo-router'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {CreateTripContext} from '../../context/CreateTripContext'
+
 
 export default function SearchPlace() {
     const navigation = useNavigation();
+    const {tripData,setTripData} = useContext(CreateTripContext);
 
     useEffect(()=>{
         navigation.setOptions({
@@ -13,7 +16,11 @@ export default function SearchPlace() {
             headerTransparent:true,
             headerTitle:'Search'
         })
-    })
+    },[]);
+
+    useEffect(()=>{
+      console.log(tripData)
+    }),[tripData];
   return (
     <View
     style={{
@@ -24,15 +31,31 @@ export default function SearchPlace() {
     }}
     >
       <GooglePlacesAutocomplete
-      placeholder='Search'
-      onPress={(data, details ) => {
-        console.log(data, details);
+      placeholder='Search Place'
+      fetchDetails ={true}
+      onPress={(data, details=null ) => {
+        console.log(details)
+        setTripData({
+          locationInfo:{
+            name:data.description,
+            coordinates:details?.geometry.location,
+            url:details?.url
+          }
+        })
       }}
       query={{
-        key: 'YOUR API KEY',
+        key: process.env.EXPO_PUBLIC_GOOGLE_MAP_KEY,
         language: 'en',
       }}
+      styles={{
+       textInputContainer:{
+        borderWidth:1,
+        borderRadius:10,
+        marginTop:10,
+       }
+      }}
     />
+
     </View>
   )
 } 
