13a4c3abd58fb9f85f8c19f09ef7781ede9ea6f3
Suraj Dey <surajdey2k1@gmail.com>
2024-12-09 23:40:33 +0530

    Update _layout.jsx

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,29 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
-import { Ionicons } from '@expo/vector-icons'
-import { Colors } from '../../constants/Colors'
-
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "../../constants/Colors";
 export default function TabLayout() {
   return (
-   <Tabs screenOptions={{headerShown:false,
-    tabBarActiveTintColor:Colors.BLACK
-   }}>
-    <Tabs.Screen name='mytrip'
-    options={{
-      tabBarIcon:({color})=><Ionicons name='location-sharp' size={24} color={color}/>
-    }}
-    />
-    <Tabs.Screen name='discover'
-     options={{
-      tabBarIcon:({color})=><Ionicons name='globe-sharp' size={24} color={color}/>
-    }}
-    />
-    <Tabs.Screen name='profile'
-     options={{
-      tabBarIcon:({color})=><Ionicons name='people-circle' size={24} color={color}/>
-    }}
-    />
-   </Tabs>
-  )
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: Colors.BLACK,
+      }}
+    >
+      <Tabs.Screen
+        name="mytrip"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="location-sharp" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="discover"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="globe-sharp" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="people-circle" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
